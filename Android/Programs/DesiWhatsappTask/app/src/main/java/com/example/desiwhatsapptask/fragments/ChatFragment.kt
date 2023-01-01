7d9cc975c4dd3647df1d0d5164d7bcef1ba91a66
Jeet Patel <jeet63240@gmail.com>
2023-01-01 11:17:38 +0530

    Start Whatsapp Clone Task

---
+++ Android/Programs/DesiWhatsappTask/app/src/main/java/com/example/desiwhatsapptask/fragments/ChatFragment.kt
@@ -0,0 +1,38 @@
+package com.example.desiwhatsapptask.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.desiwhatsapptask.Firebase.RealTimeDatabase
+import com.example.desiwhatsapptask.adapters.ChatFragmentAdapter
+import com.example.desiwhatsapptask.databinding.FragmentChatBinding
+import com.example.desiwhatsapptask.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+
+class ChatFragment : Fragment() {
+   private var lists:ArrayList<User> = ArrayList()
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var fragmentChatBinding: FragmentChatBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        mAuth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        fragmentChatBinding= FragmentChatBinding.inflate(inflater, container, false)
+        fragmentChatBinding.rvChat.layoutManager =LinearLayoutManager(requireActivity())
+        val userAdapter = ChatFragmentAdapter(this@ChatFragment, lists)
+        fragmentChatBinding.rvChat.adapter = userAdapter
+        RealTimeDatabase().loadUsers(lists, userAdapter)
+        // Inflate the layout for this fragment
+        return fragmentChatBinding.root
+    }
+
+}
